02b61e1f6975cdafb212739c79d5cccf63f489cb
yellowsea <yunhai0644@163.com>
2022-05-21 16:59:41 +0800

    123

--- test/e2e/plugins/index.js
+++ test/e2e/plugins/index.js
@@ -19,4 +19,11 @@
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+  return Object.assign({}, config, {
+    fixturesFolder: 'test/e2e/fixtures',
+    integrationFolder: 'test/e2e/specs',
+    screenshotsFolder: 'test/e2e/screenshots',
+    videosFolder: 'test/e2e/videos',
+    supportFile: 'test/e2e/support/index.js',
+  })
 }
