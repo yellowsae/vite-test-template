fcea69f084943303ace9f8457b8e12b61f325c6e
yellowsea <yunhai0644@163.com>
2022-05-22 13:49:41 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+// 引入path
+import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+
+  // 配置别名
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src/"),
+      "@c": resolve(__dirname, "src/components"),
+    },
+    // 省略后缀
+    extensions: ['.js', '.jsx', '.ts']
+  }
 })
